d38726bd16dec511b8a777e0335e2f99f27b9bbc
lucassouzapb <lycasoliveira@gmail.com>
2022-09-01 16:47:10 -0300

    Iniciando projeto

--- src/main/java/biblio/com/avaliacao/model/Autor.java
+++ src/main/java/biblio/com/avaliacao/model/Autor.java
@@ -16,20 +16,24 @@ public class Autor {
     private String nome;
 
     public long getId() {
+
         return id;
     }
 
     @NotEmpty(message="Você precisa informar o nome do autor")
     @Length(min=2, max=200, message="O nome deverá ter entre 2 e 200 caracteres")
     public String getNome() {
+
         return nome;
     }
 
     public void setNome(String nome) {
+
         this.nome = nome;
     }
 
     public void setId(long id) {
+
         this.id = id;
     }
 }
