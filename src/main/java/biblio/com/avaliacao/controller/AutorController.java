d38c01b8a67dcd4e37390318d7799b57a890786f
lucassouzapb <lycasoliveira@gmail.com>
2022-09-01 16:30:38 -0300

    Iniciando projeto

--- src/main/java/biblio/com/avaliacao/controller/AutorController.java
+++ src/main/java/biblio/com/avaliacao/controller/AutorController.java
@@ -20,7 +20,6 @@ public class AutorController {
     @Autowired
     private AutorRepository autorRepository;
 
-
     @ApiOperation(value="Mostra lista de autores")
     @GetMapping("/autor")
     public List<Autor> listarAutores() {
@@ -29,7 +28,7 @@ public class AutorController {
 
     @ApiOperation(value="Mostra um autor com base no id informado")
     @GetMapping("/autor/{id}")
-    public ResponseEntity<Autor> GetById(@PathVariable(value = "id") long id)
+    public ResponseEntity<Autor> getByIdAutor(@PathVariable(value = "id") long id)
     {
         Optional<Autor> autor = autorRepository.findById(id);
         if(autor.isPresent())
@@ -38,8 +37,9 @@ public class AutorController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
+    @ApiOperation(value="Cadastra um novo autor")
     @PostMapping("/autor")
-    public ResponseEntity<Response<Autor>> Post(@Valid @RequestBody Autor autor, BindingResult result)
+    public ResponseEntity<Response<Autor>> cadastrarAutor(@Valid @RequestBody Autor autor, BindingResult result)
     {
         Response<Autor> response = new Response<Autor>();
         if (result.hasErrors()) {
@@ -49,5 +49,40 @@ public class AutorController {
         autorRepository.save(autor);
         response.setData(autor);
         return ResponseEntity.ok(response);
+    }
+
+    @ApiOperation(value="Atualiza um autor")
+    @PutMapping("/autor")
+    public ResponseEntity<Response<Autor>> atualizarAutor(@PathVariable(value = "id") long id, @Valid @RequestBody
+    Autor novoAutor, BindingResult result)
+    {
+        Optional<Autor> antigoAutor = autorRepository.findById(id);
+        Response<Autor> response = new Response<Autor>();
+        if (result.hasErrors()) {
+            result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
+            return ResponseEntity.badRequest().body(response);
+        }
+        if(antigoAutor.isPresent()){
+            Autor autor = antigoAutor.get();
+            autor.setNome(novoAutor.getNome());
+            response.setData(autor);
+            autorRepository.save(autor);
+            return ResponseEntity.ok(response);
+        }
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @ApiOperation(value="Deleta um autor com base no id informado")
+    @DeleteMapping("/autor/{id}")
+    public ResponseEntity<Object> deletarAutor(@PathVariable(value = "id") long id)
+    {
+        Optional<Autor> autor = autorRepository.findById(id);
+        if(autor.isPresent()){
+            autorRepository.delete(autor.get());
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
