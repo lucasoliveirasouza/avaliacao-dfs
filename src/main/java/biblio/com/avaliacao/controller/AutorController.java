7370992a0e1dee9ce992dc83d9f8cb3ff66f564f
lucassouzapb <lycasoliveira@gmail.com>
2022-09-01 16:16:21 -0300

    Iniciando projeto

--- src/main/java/biblio/com/avaliacao/controller/AutorController.java
+++ src/main/java/biblio/com/avaliacao/controller/AutorController.java
@@ -2,12 +2,15 @@ package biblio.com.avaliacao.controller;
 
 import biblio.com.avaliacao.model.Autor;
 import biblio.com.avaliacao.repository.AutorRepository;
+import biblio.com.avaliacao.responses.Response;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
@@ -33,5 +36,18 @@ public class AutorController {
             return new ResponseEntity<Autor>(autor.get(), HttpStatus.OK);
         else
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/autor")
+    public ResponseEntity<Response<Autor>> Post(@Valid @RequestBody Autor autor, BindingResult result)
+    {
+        Response<Autor> response = new Response<Autor>();
+        if (result.hasErrors()) {
+            result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
+            return ResponseEntity.badRequest().body(response);
+        }
+        autorRepository.save(autor);
+        response.setData(autor);
+        return ResponseEntity.ok(response);
     }
 }
