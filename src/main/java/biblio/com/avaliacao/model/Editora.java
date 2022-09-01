747313c9961d49cc633611ab44aa14210bf1a3b4
lucassouzapb <lycasoliveira@gmail.com>
2022-09-01 16:49:13 -0300

    Iniciando projeto

--- src/main/java/biblio/com/avaliacao/model/Editora.java
+++ src/main/java/biblio/com/avaliacao/model/Editora.java
@@ -1,4 +1,39 @@
 package biblio.com.avaliacao.model;
 
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
 public class Editora {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    public long getId() {
+
+        return id;
+    }
+
+    @NotEmpty(message="Você precisa informar o nome da editora")
+    @Length(min=2, max=200, message="O nome da editora deverá ter entre 2 e 200 caracteres")
+    public String getNome() {
+
+        return nome;
+    }
+
+    public void setNome(String nome) {
+
+        this.nome = nome;
+    }
+
+    public void setId(long id) {
+
+        this.id = id;
+    }
 }
