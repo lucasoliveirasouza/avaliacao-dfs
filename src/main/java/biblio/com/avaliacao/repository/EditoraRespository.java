a15dd80ed8071ca95f174be608a51ad403208098
lucassouzapb <lycasoliveira@gmail.com>
2022-09-01 16:50:12 -0300

    Iniciando projeto

---
+++ src/main/java/biblio/com/avaliacao/repository/EditoraRespository.java
@@ -0,0 +1,7 @@
+package biblio.com.avaliacao.repository;
+
+import biblio.com.avaliacao.model.Editora;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EditoraRespository extends JpaRepository<Editora, Long> {
+}
