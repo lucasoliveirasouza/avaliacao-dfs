6993fb6b6053e3a7a12f1fcc5d5680e7eff4a86d
lucassouzapb <lycasoliveira@gmail.com>
2022-09-01 15:31:22 -0300

    Iniciando projeto

--- src/main/java/biblio/com/avaliacao/controller/AutorController.java
+++ src/main/java/biblio/com/avaliacao/controller/AutorController.java
@@ -1,4 +1,22 @@
 package biblio.com.avaliacao.controller;
 
+import biblio.com.avaliacao.model.Autor;
+import biblio.com.avaliacao.repository.AutorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class AutorController {
+
+    @Autowired
+    private AutorRepository autorRepository;
+    // A anotação @RequestMapping permite definir uma rota
+    @RequestMapping(value = "/autor", method = RequestMethod.GET)
+    public List<Autor> buscarAutores() {
+        return autorRepository.findAll();
+    }
 }
