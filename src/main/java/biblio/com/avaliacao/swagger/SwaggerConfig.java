0cc453d90e3f10acc975879df19756badd306e87
lucassouzapb <lycasoliveira@gmail.com>
2022-09-01 15:16:47 -0300

    Configurando projeto

--- src/main/java/biblio/com/avaliacao/swagger/SwaggerConfig.java
+++ src/main/java/biblio/com/avaliacao/swagger/SwaggerConfig.java
@@ -1,4 +1,9 @@
 package biblio.com.avaliacao.swagger;
 
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
 public class SwaggerConfig {
 }
