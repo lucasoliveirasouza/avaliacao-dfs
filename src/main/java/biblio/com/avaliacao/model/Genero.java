5faeaefb88304f2c0ace17107f96543eda915816
lucassouzapb <lycasoliveira@gmail.com>
2022-09-01 17:18:00 -0300

    Iniciando projeto

--- src/main/java/biblio/com/avaliacao/model/Genero.java
+++ src/main/java/biblio/com/avaliacao/model/Genero.java
@@ -1,4 +1,39 @@
 package biblio.com.avaliacao.model;
 
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
 public class Genero {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    public long getId() {
+
+        return id;
+    }
+
+    @NotEmpty(message="Você precisa informar o nome do gênero")
+    @Length(min=2, max=200, message="O nome do gênero deverá ter entre 2 e 200 caracteres")
+    public String getNome() {
+
+        return nome;
+    }
+
+    public void setNome(String nome) {
+
+        this.nome = nome;
+    }
+
+    public void setId(long id) {
+
+        this.id = id;
+    }
 }
---
+++ src/main/java/biblio/com/avaliacao/repository/GeneroRepository.java
@@ -0,0 +1,8 @@
+package biblio.com.avaliacao.repository;
+
+
+import biblio.com.avaliacao.model.Genero;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GeneroRepository extends JpaRepository<Genero, Long> {
+}
