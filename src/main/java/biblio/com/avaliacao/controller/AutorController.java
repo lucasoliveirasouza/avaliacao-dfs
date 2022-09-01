5ca5d1444e87f3dadf9d90da8e5d60fa06868d84
lucassouzapb <lycasoliveira@gmail.com>
2022-09-01 16:12:13 -0300

    Iniciando projeto

--- src/main/java/biblio/com/avaliacao/controller/AutorController.java
+++ src/main/java/biblio/com/avaliacao/controller/AutorController.java
@@ -4,12 +4,12 @@ import biblio.com.avaliacao.model.Autor;
 import biblio.com.avaliacao.repository.AutorRepository;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class AutorController {
@@ -20,7 +20,18 @@ public class AutorController {
 
     @ApiOperation(value="Mostra lista de autores")
     @GetMapping("/autor")
-    public List<Autor> buscarAutores() {
+    public List<Autor> listarAutores() {
         return autorRepository.findAll();
+    }
+
+    @ApiOperation(value="Mostra um autor com base no id informado")
+    @GetMapping("/autor/{id}")
+    public ResponseEntity<Autor> GetById(@PathVariable(value = "id") long id)
+    {
+        Optional<Autor> autor = autorRepository.findById(id);
+        if(autor.isPresent())
+            return new ResponseEntity<Autor>(autor.get(), HttpStatus.OK);
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
