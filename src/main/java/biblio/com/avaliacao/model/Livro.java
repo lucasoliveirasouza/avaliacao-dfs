f1ad9194ac936d6a97595c3ba234623b69572be1
lucassouzapb <lycasoliveira@gmail.com>
2022-09-02 10:49:36 -0300

    Iniciando projeto

--- src/main/java/biblio/com/avaliacao/model/Livro.java
+++ src/main/java/biblio/com/avaliacao/model/Livro.java
@@ -1,4 +1,41 @@
 package biblio.com.avaliacao.model;
 
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+
 public class Livro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    public long getId() {
+
+        return id;
+    }
+
+    @NotEmpty(message="Você precisa informar o nome do livro")
+    @Length(min=2, max=200, message="O nome do gênero deverá ter entre 2 e 200 caracteres")
+    public String getNome() {
+
+        return nome;
+    }
+
+    public void setNome(String nome) {
+
+        this.nome = nome;
+    }
+
+    public void setId(long id) {
+
+        this.id = id;
+    }
 }
