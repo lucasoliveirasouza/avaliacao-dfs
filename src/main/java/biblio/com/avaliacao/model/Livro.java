82c5c425e5924c19fa5d15df704093dde2719426
lucasoliveirasouza <lycasoliveira@gmail.com>
2022-09-03 13:31:22 -0300

    Ajustes

--- src/main/java/biblio/com/avaliacao/model/Livro.java
+++ src/main/java/biblio/com/avaliacao/model/Livro.java
@@ -27,36 +27,44 @@ public class Livro {
     @ManyToOne
     private Editora editora;
 
+    @ManyToOne
     private Genero genero;
 
 
     public long getId() {
+
         return id;
     }
 
     public void setId(long id) {
+
         this.id = id;
     }
 
     @NotEmpty(message="Você precisa informar o titulo do livro")
     @Length(min=2, max=200, message="O titulo do livro deverá ter entre 2 e 200 caracteres")
     public String getTitulo() {
+
         return titulo;
     }
 
     public void setTitulo(String titulo) {
+
         this.titulo = titulo;
     }
 
     public int getAnoPublicacao() {
+
         return anoPublicacao;
     }
 
     public void setAnoPublicacao(int anoPublicacao) {
+
         this.anoPublicacao = anoPublicacao;
     }
 
     public String getIsbn() {
+
         return isbn;
     }
 
@@ -64,14 +72,17 @@ public class Livro {
     @Length(min=10, max=200, message="O ISBN do livro deverá ter entre 10 e 200 caracteres")
 
     public void setIsbn(String isbn) {
+
         this.isbn = isbn;
     }
 
     public int getQuantidade() {
+
         return quantidade;
     }
 
     public void setQuantidade(int quantidade) {
+
         this.quantidade = quantidade;
     }
 }
