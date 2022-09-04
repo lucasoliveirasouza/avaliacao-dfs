0ba6f032924cc1b7fb34025f2e022642da905bf5
lucasoliveirasouza <lycasoliveira@gmail.com>
2022-09-04 13:36:38 -0300

    Ajustes

--- src/main/java/biblio/com/avaliacao/repository/EditoraRepository.java
+++ src/main/java/biblio/com/avaliacao/repository/EditoraRepository.java
@@ -4,4 +4,5 @@ import biblio.com.avaliacao.model.Editora;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EditoraRepository extends JpaRepository<Editora, Long> {
+    Boolean existsByNome(String nome);
 }
